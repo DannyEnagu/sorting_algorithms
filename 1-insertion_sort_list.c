c1556394227ac364271b2312f828cfd59786191f
dannyEnagu <danielEnagu@gmail.com>
2023-01-25 07:47:44 +0000

    Test for already sorted list

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -12,28 +12,23 @@
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *head, *current, *prev;
-	bool swaped = false;
 
 	if (*list && list && (*list)->next)
 	{
 		head = (*list)->next;
-		while (head != NULL)
+		while (head)
 		{
 			current = head;
 			prev = current->prev;
 			while (prev && current->n < prev->n)
 			{
 				swap_nodes(&prev, &current);
-				swaped = true;
 				if (prev == *list)
 					*list = current;
 				print_list(*list);
 				prev = current->prev;
 			}
 			head = head->next;
-
-			if (swaped == false)
-				return;
 		}
 	}
 }
@@ -88,10 +83,10 @@ void swap_nodes(listint_t **A, listint_t **B)
 	(*A)->next = (*B)->next;
 	(*B)->prev = (*A)->prev;
 
-	if ((*B)->next != NULL)
+	if ((*B)->next)
 		(*B)->next->prev = *A;
 
-	if ((*A)->prev != NULL)
+	if ((*A)->prev)
 		(*A)->prev->next = *B;
 
 	(*B)->next = *A;
