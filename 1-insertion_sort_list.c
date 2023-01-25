dc5b0e6a638cb14c26981d30f9f068ce666d245a
dannyEnagu <danielEnagu@gmail.com>
2023-01-25 07:01:09 +0000

    Fix betty errors

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,8 +1,5 @@
 #include "sort.h"
 
-void sorted_insert(listint_t **sorted, listint_t *new);
-void swap_nodes(listint_t **A, listint_t **B);
-
 /**
  * insertion_sort_list - Sorts a doubly linked list of integers
  *	in ascending order using the Insertion sort algorithm
@@ -16,24 +13,24 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *head, *current, *prev;
 
-	if (*list == NULL || (*list)->next == NULL)
-		return;
-
-	head = (*list)->next;
-	while (head != NULL)
+	if (*list && list && (*list)->next)
 	{
-		current = head;
-		prev = current->prev;
-		while (prev && current->n < prev->n)
+		head = (*list)->next;
+		while (head != NULL)
 		{
-			swap_nodes(&prev, &current);
-			if (prev == *list)
-				*list = current;
-			print_list(*list);
+			current = head;
 			prev = current->prev;
-		}
+			while (prev && current->n < prev->n)
+			{
+				swap_nodes(&prev, &current);
+				if (prev == *list)
+					*list = current;
+				print_list(*list);
+				prev = current->prev;
+			}
 
-		head = head->next;
+			head = head->next;
+		}
 	}
 }
 
@@ -75,12 +72,10 @@ void sorted_insert(listint_t **sorted, listint_t *new)
 }
 
 /**
-* swap_nodes - compare two node data in a linked list and
-* swap nodes, if need be.
+* swap_nodes - Swap left and right nodes
 *
-* @head: pointer to head of linked list.
-* @curr: pointer to prev node
-* @next: pointer to next node
+* @A: pointer to left node
+* @B: pointer to right node
 *
 * Return: Nothing
 */
