3543d17bd4f1fdda68e087936c6e88de444ccf64
dannyEnagu <danielEnagu@gmail.com>
2023-01-25 08:47:35 +0000

    checking insertion srot

--- sort.h
+++ sort.h
@@ -22,8 +22,6 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
-void sorted_insert(listint_t **sorted, listint_t *new);
-void swap_nodes(listint_t **A, listint_t **B);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
